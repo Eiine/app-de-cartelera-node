248ef9ed14fd27dad0586c7ff7afc902dd2f4d05
Miguel Guerrero <91755159+Eiine@users.noreply.github.com>
2022-12-14 23:25:00 -0300

    update

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(session({
   saveUninitialized:true
 
 }))
-app.use(myconn(mysql, conection))
+app.use(myconn(mysql, connection,"single"))
 app.use(express.urlencoded({extended: false}));
 app.use(express.json())
 app.use(cookieparser())
