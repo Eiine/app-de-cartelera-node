8676b3c90e37aa58aa6e07124e217d6594f74686
Miguel Guerrero <yannokaiserfrom@gmail.com>
2022-12-20 13:10:04 -0300

    correccion mysql por mysql2

--- rutas/ruta.js
+++ rutas/ruta.js
@@ -1,288 +1,280 @@
-const express=require("express")
+const express = require("express");
 
-const route=express.Router()
-const mysql=require ("mysql")
-const bcrypt=require("bcryptjs")
-const jwt=require("jsonwebtoken")
-const autorizado=require("../seguridad/autenticar")
+const route = express.Router();
+const mysql = require("mysql2");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const autorizado = require("../seguridad/autenticar");
 
 //rutas e interaccion usuario
-route.get("/",(req,res)=>{
-  res.render("index")
-})
+route.get("/", (req, res) => {
+  res.render("index");
+});
 
-route.get("/registroG",autorizado,(req,res)=>{
-  res.render("registroG")
-})
+route.get("/registroG", autorizado, (req, res) => {
+  res.render("registroG");
+});
 
-route.get("/noticias",autorizado,(req,res)=>{
-  res.render("noticias")
-})
+route.get("/noticias", autorizado, (req, res) => {
+  res.render("noticias");
+});
 
-route.get("/cartelera", async (req,res)=>{
-   await req.getConnection(async(err,conn)=>{
-    if(err) return res.send(err)
+route.get("/cartelera", (req, res) => {
+  req.getConnection((err, conn) => {
+    if (err) return res.send(err);
 
-    await conn.query("SELECT * FROM `post-noticias`",(err,users)=>{
-      if(err) return res.send(err)
-      
-      res.render("cartelera",{
-        data:users
-      })
-    })
-  })
-  
-})
+    conn.query("SELECT * FROM `post-noticias`", (err, users) => {
+      if (err) return res.send(err);
 
-route.get("/carteleraG",(req,res)=>{
-  req.getConnection((err,conn)=>{
-    if(err) return res.send(err)
+      res.render("cartelera", {
+        data: users,
+      });
+    });
+  });
+});
 
-    conn.query("SELECT * FROM `grupos`",(err,users)=>{
-      if(err) return res.send(err)
-      
-      res.render("carteleraG",{
-        data:users
-      })
-    })
-  })
-  
-})
+route.get("/carteleraG", (req, res) => {
+  req.getConnection((err, conn) => {
+    if (err) return res.send(err);
 
+    conn.query("SELECT * FROM `grupos`", (err, users) => {
+      if (err) return res.send(err);
 
+      res.render("carteleraG", {
+        data: users,
+      });
+    });
+  });
+});
 
+route.get("/login", (req, res) => {
+  res.render("login");
+});
 
-route.get("/login",(req,res)=>{
-  res.render("login")
-})
+route.get("/registroerr", (req, res) => {
+  res.render("registroerr");
+});
+route.get("/registroG", (req, res) => {
+  res.render("registroG");
+});
 
-route.get("/registroerr",(req,res)=>{
-  res.render("registroerr")
-})
-route.get("/registroG",(req,res)=>{
-  res.render("registroG")
-})
-
-route.get("/registroU",(req,res)=>{
-  res.render("registroU")
-})
+route.get("/registroU", (req, res) => {
+  res.render("registroU");
+});
 
 //control usuarios-------------------------------------
 
-route.post('/login', async (req, res) => {
+route.post("/login", async (req, res) => {
   try {
-    const {username,pass}=req.body
-    if(!username||!pass){
-      res.render("registroerr")
-    
-    }else{
-      req.getConnection((err,conn)=>{
-        if(err) return res.send(err)
-          const {username,pass}=req.body
-          conn.query("SELECT * FROM users WHERE username=?",[username],async (err,users)=>{
-          if(users.length==0|| !(await bcrypt.compare(pass, users[0].pass))){
-            console.log(users[0].pass);
-            console.log(pass);
-            res.render("registroerr")
-          }else{
-            const id=users[0].id
-            const token=jwt.sign({id:id},"secretkey")
-            
-            let cookiesOption={
-              
-              httpOnly:true
-            }
+    const { username, pass } = req.body;
+    if (!username || !pass) {
+      res.render("registroerr");
+    } else {
+      req.getConnection((err, conn) => {
+        if (err) return res.send(err);
+        const { username, pass } = req.body;
+        conn.query(
+          "SELECT * FROM users WHERE username=?",
+          [username],
+          async (err, users) => {
+            if (
+              users.length == 0 ||
+              !(await bcrypt.compare(pass, users[0].pass))
+            ) {
+              console.log(users[0].pass);
+              console.log(pass);
+              res.render("registroerr");
+            } else {
+              const id = users[0].id;
+              const token = jwt.sign({ id: id }, "secretkey");
 
-            
-                                    
-            res.cookie("jwt", token, cookiesOption)
-            res.redirect("/cartelera")
+              let cookiesOption = {
+                httpOnly: true,
+              };
+
+              res.cookie("jwt", token, cookiesOption);
+              res.redirect("/cartelera");
+            }
           }
-          })
-          
-      })
-      
-      }
-      
-  
-    
+        );
+      });
+    }
   } catch (error) {
     console.log(error);
   }
+});
 
-})
+route.get("/api/usuarios", (req, res) => {
+  req.getConnection((err, conn) => {
+    if (err) return res.send(err);
 
-route.get('/api/usuarios', (req, res) => {
-    req.getConnection((err,conn)=>{
-      if(err) return res.send(err)
+    conn.query("SELECT * FROM users", (err, users) => {
+      if (err) return res.send(err);
 
-      conn.query("SELECT * FROM users",(err,users)=>{
-        if(err) return res.send(err)
-        
-        res.send(users)      
-      })
-    })
+      res.send(users);
+    });
+  });
+});
 
-  })
-  
-  route.post('/', async(req, res) => {
-    let {name,username,pass}=req.body
-    if(!name||!username||!pass){
-     
-      res.redirect("/registroU")
-    }else{
-      try {
-        let {name,username,pass}=req.body
-        let passhash=await bcrypt.hash(pass, 8)
-        req.getConnection((err,conn)=>{
-          if(err) return res.send(err)
-            conn.query("INSERT INTO users set ?",{name:name,username:username,pass:passhash} ,(err,users)=>{
-            if(err) return res.send(err)    
+route.post("/", async (req, res) => {
+  let { name, username, pass } = req.body;
+  if (!name || !username || !pass) {
+    res.redirect("/registroU");
+  } else {
+    try {
+      let { name, username, pass } = req.body;
+      let passhash = await bcrypt.hash(pass, 8);
+      req.getConnection((err, conn) => {
+        if (err) return res.send(err);
+        conn.query(
+          "INSERT INTO users set ?",
+          { name: name, username: username, pass: passhash },
+          (err, users) => {
+            if (err) return res.send(err);
             res.redirect("/cartelera");
+          }
+        );
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+});
 
-          });
-        });
-      }catch (error){
-        console.log(error);
+route.delete("/:id", (req, res) => {
+  req.getConnection((err, conn) => {
+    if (err) return res.send(err);
+    console.log(req.body);
+    conn.query(
+      "DELETE FROM users WHERE id = ?",
+      [req.params.id],
+      (err, users) => {
+        if (err) return res.send(err);
 
+        res.send("Usuario eliminado");
       }
-    }});
-  
-  route.delete('/:id', (req, res) => {
-    req.getConnection((err,conn)=>{
-      if(err) return res.send(err)
-      console.log(req.body);
-      conn.query("DELETE FROM users WHERE id = ?",[req.params.id] ,(err,users)=>{
-        if(err) return res.send(err)
-        
-        res.send("Usuario eliminado")
-        
-      })
-    })
+    );
+  });
+});
 
-  })
+route.put("/:id", (req, res) => {
+  req.getConnection((err, conn) => {
+    if (err) return res.send(err);
+    console.log(req.params.id);
+    conn.query(
+      "UPDATE users set ? WHERE id = ?",
+      [req.body, req.params.id],
+      (err, users) => {
+        if (err) return res.send(err);
 
-  route.put('/:id', (req, res) => {
-    req.getConnection((err,conn)=>{
-      if(err) return res.send(err)
-      console.log(req.params.id);
-      conn.query('UPDATE users set ? WHERE id = ?',[req.body ,req.params.id] ,(err,users)=>{
-        if(err) return res.send(err)
-        
-        res.send("usuario actulizado")
-        
-      })
-    })
-
-  })
+        res.send("usuario actulizado");
+      }
+    );
+  });
+});
 
 //control grupos------------------------------------------------
 
+route.get("/api/grupos", (req, res) => {
+  req.getConnection((err, conn) => {
+    if (err) return res.send(err);
 
-route.get('/api/grupos', (req, res) => {
-  req.getConnection((err,conn)=>{
-    if(err) return res.send(err)
-
-    conn.query("SELECT * FROM grupos",(err,users)=>{
-      if(err) return res.send(err)
+    conn.query("SELECT * FROM grupos", (err, users) => {
+      if (err) return res.send(err);
       console.log(users);
-      if(err) return res.send(err)
-        res.send(users)
-      })
-    })
-  })
+      if (err) return res.send(err);
+      res.send(users);
+    });
+  });
+});
 
+route.post("/grupos", (req, res) => {
+  const data = req.body;
+  req.getConnection((err, conn) => {
+    if (err) return res.send(err);
 
+    conn.query("INSERT INTO grupos set ?", [data], (err, users) => {
+      const data = req.body;
 
-route.post('/grupos', (req, res) => {
-    const data=req.body;
-  req.getConnection((err,conn)=>{
-    if(err) return res.send(err)
-    
-    conn.query("INSERT INTO grupos set ?",[data] ,(err,users)=>{
-      const data=req.body;
-      
-      if(err) return res.send(err)
-        res.redirect("/cartelera")
-      
-    })
-  })
+      if (err) return res.send(err);
+      res.redirect("/cartelera");
+    });
+  });
+});
 
-})
-
-route.delete('/grupos/:id', (req, res) => {
-  
-  req.getConnection((err,conn)=>{
-    if(err) return res.send(err)
+route.delete("/grupos/:id", (req, res) => {
+  req.getConnection((err, conn) => {
+    if (err) return res.send(err);
     console.log(req.params.id);
-    conn.query('DELETE FROM grupos WHERE id = ?',[req.params.id] ,(err,users)=>{
-      if(err) return res.send(err)
-      
-      res.send("grupo eliminado")
-      
-    })
-  })
+    conn.query(
+      "DELETE FROM grupos WHERE id = ?",
+      [req.params.id],
+      (err, users) => {
+        if (err) return res.send(err);
 
-})
+        res.send("grupo eliminado");
+      }
+    );
+  });
+});
 
-route.put('/gurpos/:id', (req, res) => {
-  req.getConnection((err,conn)=>{
-    if(err) return res.send(err)
+route.put("/gurpos/:id", (req, res) => {
+  req.getConnection((err, conn) => {
+    if (err) return res.send(err);
     console.log(req.params.id);
-    conn.query('UPDATE grupos set ? WHERE id = ?',[req.body ,req.params.id] ,(err,users)=>{
-      if(err) return res.send(err)
-      
-      res.send("grupo actulizado")
-      
-    })
-  })
+    conn.query(
+      "UPDATE grupos set ? WHERE id = ?",
+      [req.body, req.params.id],
+      (err, users) => {
+        if (err) return res.send(err);
 
-})
+        res.send("grupo actulizado");
+      }
+    );
+  });
+});
 //---------------posteo de noticias---------------------------
 
-route.get('/api/vnoticias', (req, res) => {
-  req.getConnection((err,conn)=>{
-    if(err) return res.send(err)
+route.get("/api/vnoticias", (req, res) => {
+  req.getConnection((err, conn) => {
+    if (err) return res.send(err);
 
-    conn.query("SELECT * FROM `post-noticias`",(err,users)=>{
-      if(err) return res.send(err)
-      
-      if(err) return res.send(err)
-        res.send(users)
-      })
-    })
-  })
-route.post('/noticias', (req, res) => {
-  const data=req.body;
-req.getConnection((err,conn)=>{
-  if(err) return res.send(err)
-  
-  conn.query("INSERT INTO `post-noticias` set ?",[data] ,(err,users)=>{
-    const data=req.body;
-    
-    if(err) return res.send(err)
-      res.redirect("/cartelera")
-    
-  })
-})
+    conn.query("SELECT * FROM `post-noticias`", (err, users) => {
+      if (err) return res.send(err);
 
-})
+      if (err) return res.send(err);
+      res.send(users);
+    });
+  });
+});
+route.post("/noticias", (req, res) => {
+  const data = req.body;
+  req.getConnection((err, conn) => {
+    if (err) return res.send(err);
 
-route.delete('/noticias/:id', (req, res) => {
-  
-  req.getConnection((err,conn)=>{
-    if(err) return res.send(err)
+    conn.query("INSERT INTO `post-noticias` set ?", [data], (err, users) => {
+      const data = req.body;
+
+      if (err) return res.send(err);
+      res.redirect("/cartelera");
+    });
+  });
+});
+
+route.delete("/noticias/:id", (req, res) => {
+  req.getConnection((err, conn) => {
+    if (err) return res.send(err);
     console.log(req.params.id);
-    conn.query('DELETE FROM `post-noticias` WHERE id = ?',[req.params.id] ,(err,users)=>{
-      if(err) return res.send(err)
-      
-      res.send("grupo eliminado")
-      
-    })
-  })
+    conn.query(
+      "DELETE FROM `post-noticias` WHERE id = ?",
+      [req.params.id],
+      (err, users) => {
+        if (err) return res.send(err);
 
-})
+        res.send("grupo eliminado");
+      }
+    );
+  });
+});
 
-
-
-module.exports = route ;
+module.exports = route;
--- server.js
+++ server.js
@@ -1,41 +1,38 @@
-const express = require('express')
-const app = express()
-const mysql= require("mysql2")
-const myconn=require("express-myconnection")
-const connection= require("./db/db")
-const route=require("./rutas/ruta")
-const port = process.env.PORT||3000
-const path= require("path")
-const ejs=require("ejs")
-const session=require("express-session")
-const cookieparser=require("cookie-parser")
+const express = require("express");
+const app = express();
+const mysql = require("mysql2");
+const myconn = require("express-myconnection");
+const connection = require("./db/db");
+const route = require("./rutas/ruta");
+const port = process.env.PORT || 3000;
+const path = require("path");
+const ejs = require("ejs");
+const session = require("express-session");
+const cookieparser = require("cookie-parser");
 
 //seting---------------------
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname + '/view'));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname + "/view"));
 //statics file--------------------------------
-app.use(express.static(path.join(__dirname + "/public")))
-
+app.use(express.static(path.join(__dirname + "/public")));
 
 //midelwares---------------
-app.use(session({
-  secret:"123456",
-  resave:true,
-  saveUninitialized:true
-
-}))
-app.use(myconn(mysql, connection))
-app.use(express.urlencoded({extended: false}));
-app.use(express.json())
-app.use(cookieparser())
+app.use(
+  session({
+    secret: "123456",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+app.use(myconn(mysql, connection));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cookieparser());
 //Rout----------------------------------
-app.use("/", route)
-
-
-
+app.use("/", route);
 
 //Server listener-----------------------------
 app.listen(port, () => {
-  console.log(`Server corriendo en el puerto:  ${port}`)
+  console.log(`Server corriendo en el puerto:  ${port}`);
   console.log(port);
-})
+});
