cc7b37d33ea0fed6e0b8d2b20b94f2a87539475f
Miguel Guerrero <91755159+Eiine@users.noreply.github.com>
2023-02-09 01:53:25 -0300

    Update db.js

--- db/db.js
+++ db/db.js
@@ -3,7 +3,7 @@ const connection=
 {
     host: "hcontainers-us-west-121.railway.app",
     port: 7966,
-    user: "uroot",
+    user: "root",
     password: "pPLZzMyc9zXdiCXKU34l0",
     database: "railway"
  };
