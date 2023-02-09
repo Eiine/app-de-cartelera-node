a463889c305f956cbff774be8355305c7628eb64
Miguel Guerrero <91755159+Eiine@users.noreply.github.com>
2023-02-09 01:52:05 -0300

    Update db.js

--- db/db.js
+++ db/db.js
@@ -1,9 +1,9 @@
-
+mysql -hcontainers-us-west-121.railway.app -uroot -pPLZzMyc9zXdiCXKU34l0 --port 7966 --protocol=TCP railway
 const connection=
 {
     host: "hcontainers-us-west-121.railway.app",
     port: 7966,
-    user: "root",
+    user: "uroot",
     password: "pPLZzMyc9zXdiCXKU34l0",
     database: "railway"
  };
